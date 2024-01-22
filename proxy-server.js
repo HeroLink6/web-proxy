73b259a9bf25f1f8b1a53466ce8b5725c9e44de3
HeroLink6 <123016274+HeroLink6@users.noreply.github.com>
2024-01-22 11:50:26 -0800

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
+const cheerio = require('cheerio');
 
 const app = express();
 const proxy = httpProxy.createProxyServer();
@@ -8,16 +9,29 @@ const PORT = 3000;
 
 app.use('/proxy', (req, res) => {
   const targetURL = decodeURIComponent(req.url.replace('/proxy/', ''));
-  proxy.web(req, res, { target: targetURL });
-});
+  console.log('Target URL:', targetURL);
 
-// Handle redirects
-proxy.on('proxyRes', (proxyRes, req, res) => {
-  if (proxyRes.headers['location']) {
-    const redirectUrl = proxyRes.headers['location'];
-    res.header('location', redirectUrl);
-    res.sendStatus(302); // Redirect status code
-  }
+  // Proxy the request to the target server
+  proxy.web(req, res, { target: targetURL });
+
+  // Intercept the response and modify the HTML before sending it back
+  proxy.on('proxyRes', (proxyRes, req, res) => {
+    let body = '';
+    proxyRes.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    proxyRes.on('end', () => {
+      // Modify the HTML using Cheerio or any other HTML manipulation library
+      const $ = cheerio.load(body);
+
+      // Example: Add a meta tag to the head
+      $('head').append('<meta name="my-custom-meta" content="some content">');
+
+      // Send the modified HTML back to the client
+      res.send($.html());
+    });
+  });
 });
 
 app.listen(PORT, () => {
