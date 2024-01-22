fdd8151285b06ff6539ad58c97aa4a2552910b57
HeroLink6 <123016274+HeroLink6@users.noreply.github.com>
2024-01-22 12:51:20 -0800

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 const app = express();
 const proxy = httpProxy.createProxyServer();
@@ -16,14 +15,8 @@ app.use('/proxy', async (req, res) => {
     // Fetch the HTML content from the target website
     const { data } = await axios.get(targetURL);
 
-    // Modify the HTML using Cheerio or any other HTML manipulation library
-    const $ = cheerio.load(data);
-
-    // Example: Add a div with a custom message to the body
-    $('body').append('<div>This content was modified by the proxy server!</div>');
-
-    // Send the modified HTML back to the client
-    res.send($.html());
+    // Send the fetched HTML content back to the client
+    res.send(data);
   } catch (error) {
     console.error('Error fetching content:', error);
     res.status(500).send('Internal Server Error');
@@ -33,3 +26,4 @@ app.use('/proxy', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
+
